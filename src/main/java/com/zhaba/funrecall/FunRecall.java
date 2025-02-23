80dcaa6654c4bb6c422ff5d6ec2fdc43da2b0590
B4CKU <szymekstrojny@gmail.com>
2025-02-23 16:09:28 +0100

    changed mappings to yarn. added the first iteration of the effect. check FIXMEs and TODOs in the RecallEffect class

--- src/main/java/com/zhaba/funrecall/FunRecall.java
+++ src/main/java/com/zhaba/funrecall/FunRecall.java
@@ -2,23 +2,22 @@ package com.zhaba.funrecall;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FunRecall implements ModInitializer {
 	public static final String MOD_ID = "fun-recall";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final StatusEffect RECALL_EFFECT = new RecallEffect();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "recall"), RECALL_EFFECT);
 	}
 }
---
+++ src/main/java/com/zhaba/funrecall/RecallEffect.java
@@ -0,0 +1,108 @@
+package com.zhaba.funrecall;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.TeleportTarget;
+
+import java.util.Optional;
+
+public class RecallEffect extends StatusEffect {
+    protected RecallEffect() {
+        super(StatusEffectCategory.BENEFICIAL, 0xb38ef3);
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int remainingTicks, int level) {
+        return remainingTicks == 1;
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int level) {
+        //we need this code running only on the server side of things
+        if (entity.getEntityWorld().isClient) {
+            return;
+        }
+
+        //TODO: teleport vehicles as well
+        //TODO: particles when teleporting
+        //TODO: button that adds the effect
+        //TODO: interrupted when moving, hurting
+        //TODO: debuff when interrupted/add weakness, fragility, slowness etc when gaining this effect - these buffs wouldn't get removed when interrupted
+        //TODO: progress bar for the recall?
+        if (entity instanceof ServerPlayerEntity player) {
+
+            //get the position of the recalling player
+            ServerWorld world = player.server.getWorld( player.getSpawnPointDimension() );
+            BlockPos respawnPosition = player.getSpawnPointPosition();
+            float respawnAngle = player.getSpawnAngle();
+
+            //if the spawn is in a dimension that doesn't exist anymore, or we don't have a respawnPosition for some reason - spawn in the overworld
+            if( world == null || respawnPosition == null) {
+                //FIXME: change the overworld line to get the world spawn dimension of the server. will only matter in case someone uses /setworldspawn
+                world = player.getServer().getWorld(ServerWorld.OVERWORLD);
+                respawnPosition = world.getSpawnPos();
+            }
+
+            //find a good place to plop the player down - we don't want to teleport them *inside* of their bed, just next to it
+            Optional<Vec3d> targetPos = PlayerEntity.findRespawnPosition(world, respawnPosition, respawnAngle, false, true);
+
+            //if the bed is blocked, recall to the world spawn
+            if(targetPos.isEmpty()) {
+                //FIXME: same as above
+                world = player.getServer().getWorld(ServerWorld.OVERWORLD);
+                respawnPosition = world.getSpawnPos();
+            }
+            else {
+                //overwrite the old respawn position, just so i don't have to make a new variable, or make several if/else statements on the teleport line below
+                respawnPosition = new BlockPos(((int) targetPos.get().getX()), (int) targetPos.get().getY(), (int) targetPos.get().getZ());
+            }
+
+            //we play these twice, just so people you teleported away from and ones you teleported to, are aware of your recall
+            //player should only hear these once, because of directional audio, right?
+            //...right?
+            //(even if they hear it twice, the effect doesn't sound odd, might need to look into it if i'm wrong)
+            //FIXME: might need a fix for interdimensional teleports - change the world below to the player.getworld or something and TEST IT!
+            world.playSound(null, player.getBlockPos(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 0.4f, 1f);
+            player.teleport(world, respawnPosition.getX(), respawnPosition.getY(), respawnPosition.getZ(), player.getYaw(), player.getPitch());
+            world.playSound(null, player.getBlockPos(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 0.4f, 1f);
+
+        }
+    }
+
+    /*@Override
+    public void applyUpdateEffect(LivingEntity entity, int level) {
+        //if (!(entity instanceof ServerPlayerEntity)) {
+        //    return;
+        //}
+
+        if (entity.getEntityWorld().isClient) {
+            return;
+        }
+
+        if (entity instanceof ServerPlayerEntity player) {
+
+
+            ServerWorld world = player.server.getWorld( player.getSpawnPointDimension() );
+            BlockPos respawnPosition = player.getSpawnPointPosition();
+            float respawnAngle = player.getSpawnAngle();
+
+            if( world == null || respawnPosition == null) {
+                return;
+            }
+
+            Optional<Vec3d> targetPos = PlayerEntity.findRespawnPosition(world, respawnPosition, respawnAngle, false, true);
+            targetPos.ifPresent( pos->player.teleport(pos.x, pos.y, pos.z) );
+
+        }
+    }*/
+
+}
